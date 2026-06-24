c4ef8a63dbf4c4fe6e062347ff289d5222560bfe
Sharad Shinde <shindesharad9325@gmail.com>
2026-06-24 16:41:58 +0530

    Added initial migrations for the admin, cutomer and CTO account

--- MiniBank-backend/Migrations/20260604051220_SeedVaultAccount.cs
+++ MiniBank-backend/Migrations/20260604051220_SeedVaultAccount.cs
@@ -16,6 +16,14 @@ namespace MiniBankWallet.Migrations
                 INSERT INTO BankAccounts (AccountNumber, AccountType, Balance, Status, CreatedAt, UpdatedAt, UserId, Version)
                 VALUES ('VAULT001', 4, 0.00, 'Active', GETUTCDATE(), GETUTCDATE(), {adminUserId}, NEWID());
             ");
+
+            migrationBuilder.Sql($@"
+        INSERT INTO Users (Email, PasswordHash, Role, OwnerName, MobileNumber, AadharNumber, RequiresPasswordReset)
+        VALUES 
+        ('admin@minibank.com', ' ', 2, 'System Admin', '9999999999', '000000000000', 0),
+        ('shindesharad9325@gmail.com', '$2a$11$fEEGmOMi6AsWtFSa00gRhumTYoxnuj5EDzdgG82TZ0feGzOD1QZmy', 1, 'Sharad Shinde (CTO)', '8888888888', '111111111111', 0),
+        ('unitit78@gmail.com', '', 0, 'Test Customer', '7777777777', '222222222222', 0);
+    ");
         }
 
         /// <inheritdoc />
